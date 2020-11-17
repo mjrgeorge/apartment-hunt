6d113ed548a154d2924ba263f1424e10ed654a65
mjrgeorge <mjrgeorge@gmail.com>
2020-11-17 22:55:03 +0600

    team work to my store

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdMQWKybfztR2fas7Cs8S8jpszakrVPo4",
+  authDomain: "apartment-hunt-8e374.firebaseapp.com",
+  databaseURL: "https://apartment-hunt-8e374.firebaseio.com",
+  projectId: "apartment-hunt-8e374",
+  storageBucket: "apartment-hunt-8e374.appspot.com",
+  messagingSenderId: "26371219770",
+  appId: "1:26371219770:web:af1040aa5fb545a8c7f003"
+};
+
+  export default firebaseConfig;
